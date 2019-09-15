ecd0afae8b68b3cf0340e30860cb85dc8fe7e409
luoxianzhuo <luoxianzhuo49@163.com>
2019-09-15 20:10:05 +0800

    move

---
+++ src/GenerateParenthesis.java
@@ -0,0 +1,31 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by luoxianzhuo on 2019/9/13 15:09
+ *
+ * @author luoxianzhuo
+ * @copyright Copyright 2014-2017 JD.COM All Right Reserved
+ * 括号生成
+ */
+public class GenerateParenthesis {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, String cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+        if (open < max)
+            backtrack(ans, cur+"(", open+1, close, max);
+        if (close < open)
+            backtrack(ans, cur+")", open, close+1, max);
+    }
+
+}
---
+++ src/Linked/PartitionListNode.java
@@ -0,0 +1,56 @@
+package src.Linked;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by luoxianzhuo on 2019/9/12 16:23
+ *
+ * @author luoxianzhuo
+ * @copyright Copyright 2014-2017 JD.COM All Right Reserved
+ * 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+ * <p>
+ * 你应当保留两个分区中每个节点的初始相对位置
+ */
+public class PartitionListNode {
+
+    public ListNode partition(ListNode head, int x) {
+
+        // before and after are the two pointers used to create the two list
+        // before_head and after_head are used to save the heads of the two lists.
+        // All of these are initialized with the dummy nodes created.
+        ListNode beforeHead = new ListNode(0);
+        ListNode before = beforeHead;
+        ListNode afterHead = new ListNode(0);
+        ListNode after = afterHead;
+
+        while (head != null) {
+
+            // If the original list node is lesser than the given x,
+            // assign it to the before list.
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                // If the original list node is greater or equal to the given x,
+                // assign it to the after list.
+                after.next = head;
+                after = after.next;
+            }
+
+            // move ahead in the original list
+            head = head.next;
+        }
+
+        // Last node of "after" list would also be ending node of the reformed list
+        after.next = null;
+
+        // Once all the nodes are correctly assigned to the two lists,
+        // combine them to form a single list which would be returned.
+        before.next = afterHead.next;
+        AtomicInteger atomicInteger=new AtomicInteger();
+        atomicInteger.incrementAndGet();
+
+        return beforeHead.next;
+    }
+
+}
