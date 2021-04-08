526b1be44ebfe54ab47bd633d0d43844fdf59ee6
xianzhuo <xianzhuo@staff.weibo.com>
2021-04-08 17:12:07 +0800

    二分查找

--- src/Old/BinarySearch.java
+++ src/Old/BinarySearch.java
@@ -10,17 +10,19 @@ package src.Old;
 public class BinarySearch {
 
     public static int search(int[] a, int target) {
-        if (a.length <= 0) return -1;
-        int start = 0;
-        int end = a.length - 1;
-        while (start <= end) {
-            int middle = (start + end) / 2;
-            if (target == a[middle]) {
-                return middle;
-            } else if (a[middle] > target) {
-                end = middle - 1;
-            } else if (a[middle] < target) {
-                start = middle + 1;
+        if (a.length <= 0) {
+            return -1;
+        }
+        int min = 0;
+        int max = a.length - 1;
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+            if (target == a[mid]) {
+                return mid;
+            } else if (a[mid] > target) {
+                max = mid - 1;
+            } else if (a[mid] < target) {
+                min = mid + 1;
             }
         }
         return -1;
